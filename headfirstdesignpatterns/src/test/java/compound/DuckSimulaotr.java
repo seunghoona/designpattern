dadd8a95e87f43742af13bfd0e7ee4c7e24717c4
naseu <naseunghoo@naver.com>
2021-11-04 09:23:12 +0900

    새로운 행동기능의 추가( 데코레이터패턴)

--- headfirstdesignpatterns/src/test/java/compound/DuckSimulaotr.java
+++ headfirstdesignpatterns/src/test/java/compound/DuckSimulaotr.java
@@ -5,17 +5,20 @@ import org.junit.jupiter.api.Test;
 class DuckSimulaotr {
     @Test
     void duck() {
-        Quackable mallardDuck = new MallardDuck();
-        Quackable redheadDuck = new RedheadDuck();
-        Quackable rubberDuck = new RubberDuck();
-        Quackable duckCall = new DuckCall();
-        Quackable goose = new DuckGooseAdapter(new Goose());
+        Quackable mallardDuck = new QuackCounter(new MallardDuck());
+        Quackable redheadDuck = new QuackCounter(new RedheadDuck());
+        Quackable rubberDuck = new QuackCounter(new RubberDuck());
+        Quackable duckCall = new QuackCounter(new DuckCall());
+        Quackable goose = new QuackCounter(new DuckGooseAdapter(new Goose()));
 
         simulate(mallardDuck);
         simulate(redheadDuck);
         simulate(rubberDuck);
         simulate(duckCall);
         simulate(goose);
+
+        System.out.println("QuackCOunter.getQuack() = " + QuackCounter.getQuack());
+
     }
 
     private void simulate(Quackable duck) {
